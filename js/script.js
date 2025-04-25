cd32f9382245012fa11a830e288be7cc22d74d72
Ubeiden Samboni, PhD <69818646+ubeidensamboni@users.noreply.github.com>
2025-04-25 21:38:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,71 +1,70 @@
 (function ($) {
     "use strict";
-      $('.sakura-falling').sakura('start', {
+    $('.sakura-falling').sakura('start', {
         className: 'sakura',
         fallSpeed: 2.5,
         maxSize: 30,
         minSize: 9,
         newOn: 100,
-        
     });
 })(jQuery);
 
-$(document).on('click', function(){
+$(document).on('click', function () {
     document.getElementById("my_audio").play();
 });
 
-var countDownDate = new Date("May 05, 2026 11:00:00").getTime();
+// ⏳ Updated countdown date: October 24, 2026, 2:00 PM (Colombia time)
+var countDownDate = new Date("Oct 24, 2026 14:00:00 GMT-0500").getTime();
 
-var x = setInterval(function() {
+var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>" + days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("time").innerHTML = "Bless the married couple for a happy life!";
     }
 }, 1000);
 
+// 🎉 Console Messages Update
 var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 4px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
+    'background: linear-gradient(#D33106, #571402)',
+    'border: 4px solid #3E0E02',
+    'color: white',
+    'display: block',
+    'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)',
+    'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
+    'line-height: 40px',
+    'text-align: center',
+    'font-weight: bold',
+    'font-size: 32px'
 ].join(';');
 
 var styles1 = [
-    'color: #FF6C37'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
+    'color: #FF6C37',
+    'display: block',
+    'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)',
+    'line-height: 40px',
+    'font-weight: bold',
+    'font-size: 32px'
 ].join(';');
 
 var styles2 = [
-    'color: teal'
-    , 'display: block'
-    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
-    , 'line-height: 40px'
-    , 'font-weight: bold'
-    , 'font-size: 32px'
+    'color: teal',
+    'display: block',
+    'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)',
+    'line-height: 40px',
+    'font-weight: bold',
+    'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 5th May, 2026!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Sunil Tiwari', styles1, styles2);
-
+console.log('\n\n%c SAVE THE DATE: 24th October, 2026!', styles);
+console.log('%cYour presence is requested!%c\n\nWith love: Amoy & Ubeiden ❤️', styles1, styles2);
 console.log(
-    `%cShaadi me zaroor aana!\n\n`,
+    `%c¡No faltes a nuestra boda!\n\n`,
     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
+);
